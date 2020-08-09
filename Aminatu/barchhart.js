d498326da88eb12b4c3efb32fd59ff6efa2d22f6
Aminacyber <68655281+Aminacyber@users.noreply.github.com>
2020-08-09 09:49:10 +0000

    Bar chart Assignment

---
+++ Aminatu/barchhart.js
@@ -0,0 +1,34 @@
+var monthlysales=[
+    [January,1],
+    [February,2],
+    [March,3],
+    [April,4],
+    [May,5],
+    [June,6],
+    [Jul,7],
+    [August,8],
+    [September,9],
+    [October,10],
+    [November,11],
+    [December,12],
+    
+]
+function monthlysales() {
+    var data =  [ 
+     [January,1],
+    [February,2],
+    [March,3],
+    [April,4],
+    [May,5],
+    [June,6],
+    [Jul,7],
+    [August,8],
+    [September,9],
+    [October,10],
+    [November,11],
+    [December,12],
+    
+]};
+
+var options = {'title':'Monthly Sales','width':550, 'height':400};
+document.getElementById("Monthly Sales").innerHTML
