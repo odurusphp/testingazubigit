976867a156fe1a34d94a75ab52d89cdba0a0c221
Abigail Afi Gbadago <38146824+AfiMaameDufie@users.noreply.github.com>
2020-08-10 08:28:57 +0000

    Add files via upload
    "MonthlySales"

---
+++ script.js
@@ -0,0 +1,11 @@
+const monthly_Sales = () => [
+    { month: "January", amount: Math.random() * 99999 },
+
+];
+
+function buildBarChart() {
+    const DIV_ITEMS = document.querySelector(".items");
+    
+}
+
+buildBarChart();
